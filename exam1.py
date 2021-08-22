2693b3145f1305e6cc6aaea4a2148b4f329a5154
Baharka07 <89331772+Baharka07@users.noreply.github.com>
2021-08-22 10:33:05 +0430

    Add files via upload

---
+++ exam1.py
@@ -0,0 +1,18 @@
+x=int(input("Write your first chosen number ="))
+y=int(input("Write your sacond chosen number ="))
+s=x+y
+t=x-y
+z=x*y
+c=x/y
+v=x**y
+b=x**(1/2)
+n=x%y
+m=x//y
+print(s)
+print(t)
+print(z)
+print(c)
+print(v)
+print(b)
+print(n)
+print(m)
